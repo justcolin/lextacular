d648b06d13156bf9b3071705041be9cd27c3d65c
Colin Fulton <justcolin@gmail.com>
2016-04-04 02:42:08 -0400

    Implement TempGroupNode

---
+++ temp_group_node.rb
@@ -0,0 +1,11 @@
+# Copyright (C) 2016 Colin Fulton
+# All rights reserved.
+#
+# This software may be modified and distributed under the
+# terms of the three-clause BSD license. See LICENSE.txt
+# (located in root directory of this project) for details.
+
+module Lextacular
+  class TempGroupNode < GroupNode
+  end
+end
