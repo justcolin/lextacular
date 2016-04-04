efe4a7258d402288e176c0fb587893e007e9a14d
Colin Fulton <justcolin@gmail.com>
2016-04-04 02:23:08 -0400

    Add license statement

--- test/test_all.rb
+++ test/test_all.rb
@@ -1,3 +1,10 @@
+# Copyright (C) 2016 Colin Fulton
+# All rights reserved.
+#
+# This software may be modified and distributed under the
+# terms of the three-clause BSD license. See LICENSE.txt
+# (located in root directory of this project) for details.
+
 require './tet'
 require './node_test'
 require './group_node_test'
