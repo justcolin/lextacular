ea52c0260c400288a9cd1eccddd8455326de995a
Colin Fulton <justcolin@gmail.com>
2016-04-03 23:02:15 -0400

    Implement Node#to_s

---
+++ node.rb
@@ -0,0 +1,16 @@
+# Copyright (C) 2016 Colin Fulton
+# All rights reserved.
+#
+# This software may be modified and distributed under the
+# terms of the three-clause BSD license. See LICENSE.txt
+# (located in root directory of this project) for details.
+
+class Node
+  def initialize content
+    @content = content
+  end
+
+  def to_s
+    @content
+  end
+end
