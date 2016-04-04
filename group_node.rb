2ece1d8dc52db28e185e6536c8792840088ed428
Colin Fulton <justcolin@gmail.com>
2016-04-04 02:01:45 -0400

    Implement GroupNode#to_s

---
+++ group_node.rb
@@ -0,0 +1,18 @@
+# Copyright (C) 2016 Colin Fulton
+# All rights reserved.
+#
+# This software may be modified and distributed under the
+# terms of the three-clause BSD license. See LICENSE.txt
+# (located in root directory of this project) for details.
+
+module Lextacular
+  class GroupNode
+    def initialize *children
+      @children = children
+    end
+
+    def to_s
+      @children.map(&:to_s).join
+    end
+  end
+end
